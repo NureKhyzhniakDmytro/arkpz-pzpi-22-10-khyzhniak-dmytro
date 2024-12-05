e65d62556a66cef1b74d7cff6f16c7600e2d4f38
Dmytro Khyzhniak <dmytro.khyzhniak@nure.ua>
2024-12-05 23:03:56 +0200

    pract1: add remaining examples

--- Pract1/arkpz-pzpi-22-10-khyzhniak-dmytro-pract1/BadCodeExamples.ex
+++ Pract1/arkpz-pzpi-22-10-khyzhniak-dmytro-pract1/BadCodeExamples.ex
@@ -18,3 +18,21 @@ def handle_order_response(response) do
     :order_failed
   end
 end
+
+# Приклад №4
+defmodule OrderService do
+  def create_order(params) do
+    params
+    |> validate()
+    |> process_payment()
+    |> save_to_db()
+  end
+end
+
+# Приклад №5
+def place_order(params) do
+  case validate(params) do
+    :ok -> process_order(params)
+    :error -> :validation_failed
+  end
+end
--- Pract1/arkpz-pzpi-22-10-khyzhniak-dmytro-pract1/GoodCodeExamples.ex
+++ Pract1/arkpz-pzpi-22-10-khyzhniak-dmytro-pract1/GoodCodeExamples.ex
@@ -14,3 +14,29 @@ end
 # Приклад №3
 def handle_order_response(%{status: :ok}), do: :order_successful
 def handle_order_response(_), do: :order_failed
+
+# Приклад №4
+defmodule OrderService do
+  @moduledoc """
+  Модуль, що обробляє замовлення користувачів.
+  """
+  @doc """
+  Створює нове замовлення та зберігає його в базі даних.
+  """
+  def create_order(params) do
+    params
+    |> validate()
+    |> process_payment()
+    |> save_to_db()
+  end
+end
+
+# Приклад №5
+def place_order(params) do
+  with :ok <- validate(params),
+       processed_order <- process_order(params) do
+    processed_order
+  else
+    :error -> :validation_failed
+  end
+end
